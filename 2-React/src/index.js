8029287987c2fb72e2471701fb72468032b4bacc
Unknown <jrod4work@hotmail.com>
2018-10-02 17:40:20 -0600

    test

--- 2-React/src/index.js
+++ 2-React/src/index.js
@@ -1,1 +1,15 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const item1 = 'Apples';
+const item2 = 'Oranges';
+const item3 = 'Mangos';
+
+ReactDOM.render(
+  <ul>
+    <li>{item1}</li>
+    <li>{item2}</li>
+    <li>{item3}</li>
+  </ul>,
+  document.getElementById('root')
+);
