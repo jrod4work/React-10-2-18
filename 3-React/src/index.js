a9a8d50844fad5c74d3d4eb4995781db813ce879
Unknown <jrod4work@hotmail.com>
2018-10-03 08:05:16 -0600

    more

--- 3-React/src/index.js
+++ 3-React/src/index.js
@@ -2,32 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-
-// Any React components created will always be uppercase 
-
 const App = props => {
-  return (
-    <Employee name="Elizabeth" age={27} position="Sales Manager"/>
-  )
-}
-const Employee = props => (
-    <ul>
-      <li>{props.name}</li>
-      <li>{props.age}</li>
-      <li>{props.position}</li>
-    </ul>
-  );
-
-  Employee.propTypes = {
-    name: PropTypes.string.isRequired,
-    age: PropTypes.number.isRequired,
-    position: PropTypes.string
+  const user = {
+    name: 'George Bailey',
+    address: '1122 Drafty House',
+    city: 'Bedford Falls',
+    state: 'New York',
+    zip: 13148
   };
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+  return <User person={user} items="Boots, Tie, Socks" amount={100} />;
+};
+const User = props => (
+  <ul>
+    <li>Name: {props.person.name}</li>
+    <ul>
+      <li>Address: {props.person.address}</li>
+      <li>City: {props.person.city}</li>
+      <li>State: {props.person.state}</li>
+      <li>Zip: {props.person.zip}</li>
+    </ul>
+    <li>Items: {props.items}</li>
+    <li>Paid: {props.paid}</li>
+    <li>Amount: {props.amount}</li>
+    <li>Shipped: {props.shipped}</li>
+  </ul>
 );
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+  zip: PropTypes.number.isRequired,
+  items: PropTypes.string.isRequired,
+  paid: PropTypes.bool,
+  amount: PropTypes.number.isRequired,
+  shipped: PropTypes.bool
+};
+
+User.defaultProps = {
+  paid: false,
+  shipped: false
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+// Second example
+
+// const App = props => {
+//   return (
+//     <Employee name="Elizabeth" age={27} position="Sales Manager"/>
+//   )
+// }
+// const Employee = props => (
+//     <ul>
+//       <li>{props.name}</li>
+//       <li>{props.age}</li>
+//       <li>{props.position}</li>
+//     </ul>
+//   );
+
+//   Employee.propTypes = {
+//     name: PropTypes.string.isRequired,
+//     age: PropTypes.number.isRequired,
+//     position: PropTypes.string
+//   };
+
+//   Employee.defaultProps = {
+//     position: "employee"
+//   }
+
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('root')
+// );
 
 //1st example*****************
 
--- instructions.md
+++ instructions.md
@@ -10,7 +10,15 @@ Add these two files in the src/index.js file
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Any React components created will always be uppercase 
 
 In order for Gitkraken to ignore node_modules_
 
 touch .gitignore && echo "node_modules/" >> .gitignore
+
+Prop Validation
+
+npm install --save prop-types
+
+import PropTypes from 'prop-types';
+
