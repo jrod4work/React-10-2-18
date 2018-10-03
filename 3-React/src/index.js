118adf73a2c25af45be131686997dad64960f1b5
Johnny Rodriguez <jrod4work@hotmail.com>
2018-10-03 07:35:57 -0600

    prop validation

--- 3-React/src/index.js
+++ 3-React/src/index.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
 
 // Any React components created will always be uppercase 
 
 const App = props => {
-    return (
-      <div>
-        <Hello name="Nikki" />
-        <Hello name="Michael" />
-        <Hello name="Andrew" />
-      </div>
-    );
-  };
-  
-  const Hello = props => (
-    <div>
-      <h1>Hello, {props.name}!</h1>
-    </div>
+  return (
+    <Employee name="Elizabeth" age={27} position="Sales Manager"/>
+  )
+}
+const Employee = props => (
+    <ul>
+      <li>{props.name}</li>
+      <li>{props.age}</li>
+      <li>{props.position}</li>
+    </ul>
   );
+
+  Employee.propTypes = {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    position: PropTypes.string
+  };
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
+
+//1st example*****************
+
+// const App = props => {
+//     return (
+//       <div>
+//         <Hello name="Nikki" />
+//         <Hello name="Michael" />
+//         <Hello name="Andrew" />
+//       </div>
+//     );
+//   };
   
-  ReactDOM.render(<App />, document.getElementById('root'));
+//   const Hello = props => (
+//     <div>
+//       <h1>Hello, {props.name}!</h1>
+//     </div>
+//   );
+  
+//   ReactDOM.render(<App />, document.getElementById('root'));
   
