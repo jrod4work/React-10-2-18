b5f8411520634f12964e7675891fa6749398ddb9
Unknown <jrod4work@hotmail.com>
2018-10-03 08:54:32 -0600

    Producing JSX from Collections

--- 3-React/src/index.js
+++ 3-React/src/index.js
@@ -2,50 +2,70 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
+
+
+//MAP
+
 const App = props => {
-  const user = {
-    name: 'George Bailey',
-    address: '1122 Drafty House',
-    city: 'Bedford Falls',
-    state: 'New York',
-    zip: 13148
-  };
-  return <User person={user} items="Boots, Tie, Socks" amount={100} />;
-};
-const User = props => (
-  <ul>
-    <li>Name: {props.person.name}</li>
-    <ul>
-      <li>Address: {props.person.address}</li>
-      <li>City: {props.person.city}</li>
-      <li>State: {props.person.state}</li>
-      <li>Zip: {props.person.zip}</li>
-    </ul>
-    <li>Items: {props.items}</li>
-    <li>Paid: {props.paid}</li>
-    <li>Amount: {props.amount}</li>
-    <li>Shipped: {props.shipped}</li>
-  </ul>
-);
+  const items = ['Bread', 'Milk', 'Eggs', 'Tea'];
 
-User.propTypes = {
-  name: PropTypes.string.isRequired,
-  address: PropTypes.string.isRequired,
-  city: PropTypes.string.isRequired,
-  state: PropTypes.string.isRequired,
-  zip: PropTypes.number.isRequired,
-  items: PropTypes.string.isRequired,
-  paid: PropTypes.bool,
-  amount: PropTypes.number.isRequired,
-  shipped: PropTypes.bool
+  return <List items={items} />;
 };
 
-User.defaultProps = {
-  paid: false,
-  shipped: false
+const List = props => {
+  const listItems = props.items.map((item, index) => (
+    <li key={index}>{item}</li>
+  ));
+  return <ul>{listItems}</ul>;
 };
-
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// 3rd Example
+// const App = props => {
+//   const user = {
+//     name: 'George Bailey', 
+//     address: '1122 Drafty House',
+//     city: 'Bedford Falls',
+//     state: 'New York',
+//     zip: 13148
+//   };
+//   return <User person={user} items="Boots, Tie, Socks" amount={100} />;
+// };
+// const User = props => (
+//   <ul>
+//     <li>Name: {props.person.name}</li>
+//     <ul>
+//       <li>Address: {props.person.address}</li>
+//       <li>City: {props.person.city}</li>
+//       <li>State: {props.person.state}</li>
+//       <li>Zip: {props.person.zip}</li>
+//     </ul>
+//     <li>Items: {props.items}</li>
+//     <li>Paid: {props.paid}</li>
+//     <li>Amount: {props.amount}</li>
+//     <li>Shipped: {props.shipped}</li>
+//   </ul>
+// );
+
+// User.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   address: PropTypes.string.isRequired,
+//   city: PropTypes.string.isRequired,
+//   state: PropTypes.string.isRequired,
+//   zip: PropTypes.number.isRequired,
+//   items: PropTypes.string.isRequired,
+//   paid: PropTypes.bool,
+//   amount: PropTypes.number.isRequired,
+//   shipped: PropTypes.bool
+// };
+
+// User.defaultProps = {
+//   paid: false,
+//   shipped: false
+// };
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
