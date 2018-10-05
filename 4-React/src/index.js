20b5aa5e1b0684a63f89e76f5168d7d64f857ea4
Unknown <jrod4work@hotmail.com>
2018-10-05 07:53:43 -0600

    Project 4

--- 4-React/src/index.js
+++ 4-React/src/index.js
@@ -1,25 +1,118 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default class Layout extends React.Component {
-    constructor() {
-        super();
-        this.state = {name: "Will"};
-    }
-    render () {
-        setTimeout(() => {
-            this.setState({name: "Bob"});
-        }, 2000)
-        return (
-            <div>
-                {this.state.name}
-            </div>    
-        );
-    }
+// 3nd Example////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-}
+const App = props => {
+    return <NumberOfStudents />;
+  };
+  class NumberOfStudents extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        enrolledStudents: 0,
+        addAmount: 0,
+        waitlistedStudents:0,
+        addAmount2: 0
+      };
+     
+    }
+  
+    incrementplus1() {
+        this.setState({
+          enrolledStudents: this.state.enrolledStudents + parseInt(this.state.addAmount + 1),
+        });
+      }
+    incrementplus2() {
+        this.setState({
+          waitlistedStudents: this.state.waitlistedStudents + parseInt(this.state.addAmount + 1),
+        });
+      }
+    increment() {
+      this.setState({
+        enrolledStudents: this.state.enrolledStudents + parseInt(this.state.addAmount),
+      });
+    }
+    increment2() {
+        this.setState({
+            waitlistedStudents: this.state.waitlistedStudents + parseInt(this.state.addAmount),
+        });
+      }
+   
 
-ReactDOM.render(<Layout />, document.getElementById('root'));
+   
+    render() {
+      return (
+        <div>
+          <h3>Enrolled Students: {this.state.enrolledStudents}</h3>
+          <h3>Wait List Students: {this.state.waitlistedStudents}</h3>
+        <label>Enrolled Students by 1: </label>
+        <button onClick={this.incrementplus1.bind(this)}>Increase Enrolled Students</button>
+        <br></br>
+        <label>Increase Wait List Students by 1: </label>
+        <button onClick={this.incrementplus2.bind(this)}>Increase Wait List Students</button>
+        <br></br>
+
+          <input
+            type="number"
+            onChange={event => this.setState({ addAmount: event.target.value })}
+            value={this.state.addAmount}
+          />
+        <button onClick={this.increment.bind(this)}>Increase Enrolled Students</button>
+
+        <br></br>
+          <input
+            type="number"
+            onChange={event => this.setState({ addAmount2: event.target.value })}
+            value={this.state.addAmount2}
+          />
+          <button onClick={this.increment2.bind(this)}>Increase Wait List Students</button>
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// 2nd Example////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// const App = props => {
+//   return <BankAccount />;
+// };
+
+// class BankAccount extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       accountBalance: 2222.22,
+//       addAmount: 0
+//     };
+//   }
+
+//   increment() {
+//     this.setState({
+//       accountBalance: this.state.accountBalance + parseInt(this.state.addAmount)
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h3>Account Balance: ${this.state.accountBalance}</h3>
+//         <input
+//           type="number"
+//           onChange={event => this.setState({ addAmount: event.target.value })}
+//           value={this.state.addAmount}
+//         />
+//         <button onClick={this.increment.bind(this)}>Increase Amount</button>
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
