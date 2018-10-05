8381072d3590678a8c222292f34133c643a9fc69
Unknown <jrod4work@hotmail.com>
2018-10-05 09:00:59 -0600

    Routing with React

--- 5-React/src/index.js
+++ 5-React/src/index.js
@@ -3,3 +3,22 @@ import ReactDOM from 'react-dom';
 import Home from './screens/Home';
 import About from './screens/About';
 import Topics from './screens/Topics';
+
+const Index = ({ pathname }) => {
+    switch (pathname) {
+      case '/about':
+        return <About />;
+      case '/topics':
+        return <Topics />;
+      default:
+        return <Home />;
+    }
+  };
+  
+  let pathname = window.location.pathname;
+  
+  ReactDOM.render(<Index pathname={pathname} />, document.getElementById('root'));
+  
+  window.addEventListener('popstate', () => {
+    pathname = window.location.pathname;
+  });
